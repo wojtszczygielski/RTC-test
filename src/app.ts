3c07a62fd18d3d404415fe7491b30ed2c2c597a0
wojtszczygielski <wojtszczygielski@gmail.com>
2025-05-09 11:02:20 +0200

    feat: add simulation service

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express, { Express } from "express";
+import { SimulationService } from "./services/simulationService";
 
 export function createApp() {
+  const simulationService = new SimulationService();
+
   const app: Express = express();
   app.use(express.json());
   app.get("/health", (req, res) => {
