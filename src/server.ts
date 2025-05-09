685e594bdc4c495be71bcb7cfc2c608567092eda
wojtszczygielski <wojtszczygielski@gmail.com>
2025-05-09 11:22:35 +0200

    feat: add system signals

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { createApp } from "./app";
 import { getConfig } from "./config";
 import dotenv from "dotenv";
+import { logger } from "./utils/logger";
 
 dotenv.config();
 
@@ -10,6 +11,24 @@ const { app, eventService } = createApp();
 const server = app.listen(config.port, () => {
   console.log(`Server started on port ${config.port}`);
   eventService.startPolling();
+});
+
+process.on("SIGTERM", () => {
+  logger.info("SIGTERM received, shutting down gracefully");
+  eventService.stopPolling();
+  server.close(() => {
+    logger.info("Server closed");
+    process.exit(0);
+  });
+});
+
+process.on("SIGINT", () => {
+  logger.info("SIGINT received, shutting down gracefully");
+  eventService.stopPolling();
+  server.close(() => {
+    logger.info("Server closed");
+    process.exit(0);
+  });
 });
 
 export default server;
